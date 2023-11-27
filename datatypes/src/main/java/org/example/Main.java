6f3433018cb22749e2f8a107d06ef8db75bb828a
Esra AKBULUT <56135111+esrasrtkara@users.noreply.github.com>
2023-11-27 13:31:47 +0300

    learn datatypes

---
+++ datatypes/src/main/java/org/example/Main.java
@@ -0,0 +1,15 @@
+package org.example;
+
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class Main {
+    public static void main(String[] args) {
+        // java tip güvenli bir dildir.
+        //Değişkenlerin bellekte yer tutmasına göre değişir.
+        int sayi = 12;
+        double sayi2 = 12.5;
+        char karakter = 'A';
+        String sehir = "Ankara";
+        boolean doğruMu = true;
+    }
+}
