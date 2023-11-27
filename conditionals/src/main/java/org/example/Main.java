7e2d74e7b415cbd0d5a7d6f1921b8987061b6ab2
Esra AKBULUT <56135111+esrasrtkara@users.noreply.github.com>
2023-11-27 13:48:11 +0300

    if blokları

---
+++ conditionals/src/main/java/org/example/Main.java
@@ -0,0 +1,18 @@
+package org.example;
+
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class Main {
+    public static void main(String[] args) {
+
+        int sayi = 20;
+        if(sayi < 20){
+            System.out.println("Sayi 20 den küçüktür.");
+        }else if(sayi == 20){
+            System.out.println("Sayi 20 ye eşittir");
+        }else {
+            System.out.println("Sayi 20 den büyüktür");
+        }
+
+    }
+}
